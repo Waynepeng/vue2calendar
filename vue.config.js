9a2a1d02373c3b93b406fa6bff56d29ad96cb2f8
Waynepeng <276110220@qq.com>
2025-05-15 09:27:14 +0800

    修复 vue.config.js 配置冲突，合并静态资源路径配置

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,35 @@
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/calendar/'  // 例如：'/my-vue2-project/'
+//     : '/'
+// }
+
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
-
-
-
-module.exports = {
+  transpileDependencies: true,
+  
+  // 部署路径配置
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/calendar/'  // 例如：'/my-vue2-project/'
-    : '/'
-}
+    ? '/'  // 如果项目部署在根目录，就用 '/'
+    : '/',
+  
+  // 静态资源输出配置
+  assetsDir: '',  // 将静态资源输出到根目录
+  
+  // 自定义 Webpack 配置
+  chainWebpack: config => {
+    // 修改 JS 和 CSS 的输出路径
+    config.output.filename('[name].[contenthash].js');
+    config.output.chunkFilename('[name].[contenthash].js');
+    
+    config.plugin('extract-css').tap(() => [{
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[name].[contenthash].css'
+    }]);
+  }
+})
